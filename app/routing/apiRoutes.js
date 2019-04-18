23ff8f4ebeb0e4483800184a72d0d6cc58b318ad
David Giles <mrgteaches@live.com>
2019-04-18 11:10:44 -0400

    More changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,11 @@
 // Pull in required dependencies
-// var path = require('path');
+var path = require('path');
 
 // Import the list of friend entries
-var friends = require('../data/friends');
+var friends = require('../data/friends.js');
 
 // Export API routes
 module.exports = function(app) {
-	console.log('___ENTER apiRoutes.js___');
-
 	// Total list of friend entries
 	app.get('/api/friends', function(req, res) {
 		res.json(friends);
@@ -26,13 +24,13 @@ module.exports = function(app) {
 		// Compute best friend match
 		var matchName = '';
 		var matchImage = '';
-		var totalDifference = 10000; // Make the initial value big for comparison
+		var totalDifference = 10000; 
 
 		// Examine all existing friends in the list
 		for (var i = 0; i < friends.length; i++) {
 			// console.log('friend = ' + JSON.stringify(friends[i]));
 
-			// Compute differenes for each question
+			// Compute differences for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
